8fe0388ba3905aebd4f55975a470bbb6d60fd0e2
OlenaSkibinska <olenka.notfair.skibinska@gmail.com>
2018-05-10 10:49:43 +0300

    Changed style for events page

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,37 +1,53 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 import "../../../styles/event.less";
+
 /**
  * Represents view of single Event
  */
 const Event = ({
-  id,
-  name,
-  description,
-  date,
-  location,
-  organization,
-  photo
-}) => (
-  <div className="event__container ">
-    <div>
-      <img src={photo} className="event__photo" />
+                   id,
+                   name,
+                   description,
+                   date,
+                   location,
+                   organization,
+                   photo
+               }) => (
+    <div className="event__container ">
+        <div>
+            <img src={photo} className="event__photo"/>
+        </div>
+        <div className="event__info">
+            <p className="event__name">{name}</p>
+            <p className="event__organization">{organization}</p>
+            <div className="event__location-block">
+                <img className="event__icon-location"/>
+                <p className="event__location">{location}</p>
+            </div>
+        </div>
+        <div>
+            <NavLink
+                to={'/' + id}
+            >
+                <button className="event__btn">Show more</button>
+            </NavLink>
+        </div>
+        {/*<p className="event__location">{location}</p>*/}
+        {/*<p className="event__time">{date.toFormat("HH:mm")}</p>*/}
+        {/*<NavLink*/}
+        {/*to={"/" + id}*/}
+        {/*activeStyle={{*/}
+        {/*textDecoration: "none",*/}
+        {/*color: "black"*/}
+        {/*}}*/}
+        {/*className="event__name"*/}
+        {/*>*/}
+        {/*{name}*/}
+        {/*</NavLink>*/}
+        {/*<p className="event__organization">{organization}</p>*/}
     </div>
-    <p className="event__location">{location}</p>
-    {/*<p className="event__time">{date.toFormat("HH:mm")}</p>*/}
-    <NavLink
-      to={"/" + id}
-      activeStyle={{
-        textDecoration: "none",
-        color: "black"
-      }}
-      className="event__name"
-    >
-      {name}
-    </NavLink>
-    <p className="event__organization">{organization}</p>
-  </div>
 );
 
 export default Event;
