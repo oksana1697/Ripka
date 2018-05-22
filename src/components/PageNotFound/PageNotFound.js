e93a5b13b35927f99650c041d7e3aaefcaf3b939
OlenaSkibinska <olenka.notfair.skibinska@gmail.com>
2018-05-23 00:25:29 +0300

    Added style for PageNotFound component

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+import '../../../styles/page-not-found.less'
+import '../../../styles/common.less'
+
 class PageNotFound extends Component {
     render() {
         return (
-            <div>
-                <h1>404</h1>
-                <h2>Page not found</h2>
-                <p>Whoops!</p>
-                <p>Unfortunately there is nothing here...</p>
+            <div className='page-not-found'>
+                <h1 className='page-not-found__title'>404</h1>
+                <h2 className='page-not-found__sub-title'>Page not found</h2>
+                <p><span>Whoops!</span> Unfortunately there is nothing here...</p>
                 <Link to="/">
                     <button>
                         Back to Home
